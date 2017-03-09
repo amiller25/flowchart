8db2af534a85bcde2771c83a1ff60a43ab73f62e
amiller25 <milleraj@yelp.com>
2017-03-09 10:57:20 -0800

    Add files via upload

---
+++ HackathonFlowchart/flowchart.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(document).ready (function() {
+
+var curLoc = document.getElementById('active');
+var prevLoc;
+    
+});
+
+/*
+onclick op1
+curLoc becomes prevLoc
+.ques becomes curLoc
+remove .active from prevLoc
+add .active class to curLoc
+*/
